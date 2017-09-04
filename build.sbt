69c1edf7e8d0e73450b1062a588f027d60582f9e
yesidbotero <yesidboterom@gmail.com>
2017-09-04 16:37:45 -0500

    FlatSpec replaced by FunSuite

--- build.sbt
+++ build.sbt
@@ -7,7 +7,6 @@ scalaVersion := "2.12.3"
 libraryDependencies ++= Seq (
   "com.typesafe.akka"     %% "akka-stream"          % "2.4.17",
   "com.typesafe.akka"     %% "akka-stream-testkit"  % "2.4.17"  % "test",
-  "org.scalactic"         %% "scalactic"            % "3.0.1",
   "org.scalatest"         %% "scalatest"            % "3.0.1" % "test"
 )
 
--- src/test/scala/BasicSuite.scala
+++ src/test/scala/BasicSuite.scala
@@ -2,48 +2,36 @@ import akka.{Done, NotUsed}
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl._
-import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.FunSuite
+import scala.concurrent.duration.Duration
 
-import scala.concurrent.Future
+import scala.concurrent.{Await, Future}
 
-class BasicSuite extends FlatSpec with Matchers {
-   implicit val actorSystem = ActorSystem("system")
-   implicit val materializer = ActorMaterializer()
+class BasicSuite extends FunSuite {
+  implicit val actorSystem = ActorSystem("ActorsSystem")
+  implicit val materializer = ActorMaterializer()
 
-   "A Source " should "emit elements on demand" in {
-      val source: Source[Int, NotUsed] = Source(1 to 10)
-      source.runForeach(println)
-   }
+  test("A Source emits elements on demand") {
+    val source: Source[Int, NotUsed] = Source(1 to 10)
 
-   it should "connect to a sink" in {
-      val sink: Sink[Int, Future[Done]] = Sink.foreach(x => println("receiving: " + x))
-      val source: Source[Int, NotUsed] = Source(10 to 20)
-      source.runWith(sink)
-   }
-
-   it should "connect to a flow" in {
-     val source: Source[Int, NotUsed] = Source(1 to 20)
-     val sink: Sink[Int, Future[Done]] = Sink.foreach(x => println("receiving: " + x))
-     val flow: Flow[Int, Int, NotUsed] = Flow[Int].filter(x => x % 2 == 0)
-      //para pegarle un flow a un source y generar una fuente datos se hace con el método via
-
-     //Una fuente por un flujo a un sumidero
-     val graph = source via flow to sink
-     graph.run()
-   }
-
-    it should "connect to a flow 2" in {
-      val source = Source(10 to 20)
-      val y: (Int) => String = (n: Int) => if (n % 2 == 0) "par" else "impar"
-
-    val flow = Flow[Int].map{ y }
-
-      val x: PartialFunction[Int, String] = { case n if n % 2 == 0 => "Par"
-      case _ => "Impar"}
-
-      val z: PartialFunction[Int, String] = { case n if n % 2 == 0 => "Par"}
-
-
-    source.via(flow).runForeach(println)
+    assertCompiles("source.runForeach(println)")
   }
+
+  test("A Source should be able to run privideing it a sink") {
+    val sink: Sink[Int, Future[Done]] = Sink.foreach(x => println("receiving: " + x))
+    val source: Source[Int, NotUsed] = Source(10 to 20)
+
+    assertCompiles("source.runWith(sink)")
+  }
+
+  test("A Source connects to a flow") {
+    val source: Source[Int, NotUsed] = Source(1 to 3)
+    val isPairFlow: Flow[Int, String, NotUsed] = Flow[Int].map { case n if n % 2 == 0 => "Par "
+    case _ => "Impar " }
+    val sink: Sink[String, Future[String]] = Sink.fold("")(_ + _)
+    val result: Future[String] = source.via(isPairFlow).runWith(sink)
+
+    assert(Await.result(result, Duration.Inf) == "Impar Par Impar ")
+  }
+
 }
