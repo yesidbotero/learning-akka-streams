9bc99d7336899653f7702ef11dae10fdcc03b7e4
yesidbotero <yesidboterom@gmail.com>
2017-09-11 12:28:05 -0500

    bidiflow from functions test added

--- src/test/scala/GraphSuite.scala
+++ src/test/scala/GraphSuite.scala
@@ -3,6 +3,7 @@ import java.nio.ByteOrder
 import org.scalatest.FunSuite
 import akka.{Done, NotUsed}
 import akka.actor.ActorSystem
+import akka.actor.Status.Success
 import akka.stream._
 import akka.stream.scaladsl._
 import akka.stream.stage.{GraphStage, GraphStageLogic, InHandler, OutHandler}
@@ -149,117 +150,42 @@ class GraphSuite extends FunSuite {
   }
 
 
-  //http://doc.akka.io/docs/akka/current/scala/stream/stream-graphs.html
-  test("bidirectional flows") {
-    trait Message
-    case class Ping(id: Int) extends Message
-    case class Pong(id: Int) extends Message
+  test("defining a bidiflow from functions and using it for the RunnableGraph construction") {
+    import scala.util.Try
+    implicit val actorSystem = ActorSystem("SYSTEM")
+    implicit val materializer = ActorMaterializer();
 
-    def toBytes(msg: Message): ByteString = {
-      implicit val order = ByteOrder.LITTLE_ENDIAN
-      msg match {
-        case Ping(id) => ByteString.newBuilder.putByte(1).putInt(id).result()
-        case Pong(id) => ByteString.newBuilder.putByte(2).putInt(id).result()
-      }
+    val list = List(0, 1, 2, 3)
+
+    def mult(num: Int): Try[Int] = Try(10 * num)
+
+    def div(num: Int): Try[Int] = Try(10 / num).recoverWith {
+      case e: Exception => Try(-1)
     }
 
-    def fromBytes(bytes: ByteString): Message = {
-      implicit val order = ByteOrder.LITTLE_ENDIAN
-      val it = bytes.iterator
-      it.getByte match {
-        case 1 => Ping(it.getInt)
-        case 2 => Pong(it.getInt)
-        case other => throw new RuntimeException(s"parse error: expected 1|2 got $other")
-      }
-    }
+    val sink = Sink.fold(List.empty[Try[Int]])((list: List[Try[Int]], element: Try[Int]) => element :: list)
 
-    val codecVerbose = BidiFlow.fromGraph(GraphDSL.create() { b =>
-      // construct and add the top flow, going outbound
-      val outbound = b.add(Flow[Message].map(toBytes))
-      // construct and add the bottom flow, going inbound
-      val inbound = b.add(Flow[ByteString].map(fromBytes))
-      // fuse them together into a BidiShape
-      BidiShape.fromFlows(outbound, inbound)
-    })
+    val grp = RunnableGraph.fromGraph(GraphDSL.create(sink, sink)((_, _)) {
+      implicit builder =>
+        (sk1, sk2) => {
+          import GraphDSL.Implicits._
 
-    // this is the same as the above
-    val codec = BidiFlow.fromFunctions(toBytes _, fromBytes _)
+          val s1 = builder.add(Source(list))
+          val s2 = builder.add(Source(list.reverse))
+          val bidi: BidiShape[Int, Try[Int], Int, Try[Int]] = builder.add(BidiFlow.fromFunctions(mult, div))
 
+          s1 ~> bidi.in1
+          bidi.out1 ~> sk1
+          bidi.in2 <~ s2
+          sk2 <~ bidi.out2
 
-    val framing = BidiFlow.fromGraph(GraphDSL.create() { b =>
-      implicit val order = ByteOrder.LITTLE_ENDIAN
-
-      def addLengthHeader(bytes: ByteString) = {
-        val len = bytes.length
-        ByteString.newBuilder.putInt(len).append(bytes).result()
-      }
-
-      class FrameParser extends GraphStage[FlowShape[ByteString, ByteString]] {
-
-        val in = Inlet[ByteString]("FrameParser.in")
-        val out = Outlet[ByteString]("FrameParser.out")
-        override val shape = FlowShape.of(in, out)
-
-        override def createLogic(inheritedAttributes: Attributes): GraphStageLogic = new GraphStageLogic(shape) {
-
-          // this holds the received but not yet parsed bytes
-          var stash = ByteString.empty
-          // this holds the current message length or -1 if at a boundary
-          var needed = -1
-
-          setHandler(out, new OutHandler {
-            override def onPull(): Unit = {
-              if (isClosed(in)) run()
-              else pull(in)
-            }
-          })
-          setHandler(in, new InHandler {
-            override def onPush(): Unit = {
-              val bytes = grab(in)
-              stash = stash ++ bytes
-              run()
-            }
-
-            override def onUpstreamFinish(): Unit = {
-              // either we are done
-              if (stash.isEmpty) completeStage()
-              // or we still have bytes to emit
-              // wait with completion and let run() complete when the
-              // rest of the stash has been sent downstream
-              else if (isAvailable(out)) run()
-            }
-          })
-
-          private def run(): Unit = {
-            if (needed == -1) {
-              // are we at a boundary? then figure out next length
-              if (stash.length < 4) {
-                if (isClosed(in)) completeStage()
-                else pull(in)
-              } else {
-                needed = stash.iterator.getInt
-                stash = stash.drop(4)
-                run() // cycle back to possibly already emit the next chunk
-              }
-            } else if (stash.length < needed) {
-              // we are in the middle of a message, need more bytes,
-              // or have to stop if input closed
-              if (isClosed(in)) completeStage()
-              else pull(in)
-            } else {
-              // we have enough to emit at least one message, so do it
-              val emit = stash.take(needed)
-              stash = stash.drop(needed)
-              needed = -1
-              push(out, emit)
-            }
-          }
+          ClosedShape
         }
-      }
-
-      val outbound = b.add(Flow[ByteString].map(addLengthHeader))
-      val inbound = b.add(Flow[ByteString].via(new FrameParser))
-      BidiShape.fromFlows(outbound, inbound)
     })
+
+    val result: (Future[List[Try[Int]]], Future[List[Try[Int]]]) = grp.run()
+
+    assert(Await.result(result._1, Duration.Inf).map(x => x.get) == List(30, 20, 10, 0))
+    assert(Await.result(result._2, Duration.Inf).map(x => x.get) == List(-1, 10, 5, 3))
   }
 }
